22bb4f67f2b57b73c2fc43f33ea12148f9a2baa6
Mingming Chen <naivechen02@gmail.com>
2015-02-11 15:13:19 +0800

    easy

---
+++ A + B 问题.cpp
@@ -0,0 +1,15 @@
+// http://lintcode.com/zh-cn/problem/a-b-problem/
+
+
+class Solution {
+public:
+    /*
+     * @param a: The first integer
+     * @param b: The second integer
+     * @return: The sum of a and b
+     */
+    int aplusb(int a, int b) {
+        // Just submit this code, then you will get accepted!
+        return a + b;
+    }
+};
